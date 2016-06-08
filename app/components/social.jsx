d52ebf9adfad8eeb0c592a3f69c4488a528ff2bd
Keith Richards <dkrichards86@gmail.com>
2016-06-08 23:40:08 +0000

    Typo cleanup

--- app/components/social.jsx
+++ app/components/social.jsx
@@ -22,7 +22,7 @@ export default class Social extends React.Component {
         return (
             <aside>
                 <i className="menu-icon" onClick={this.handleClick.bind(this)} />
-                <div className="soclial-links" rel="social">
+                <div className="social-links" ref="social">
                     <a href="https://github.com/dkrichards86/" target="_blank"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
                     <a href="http://codepen.io/dkrichards86/" target="_blank"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
                     <a href="www.linkedin.com/in/dkrichards" target="_blank"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
