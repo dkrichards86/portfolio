c2d8c05ea5cb3bf77486c671702b3a8aeb968d02
Keith Richards <dkrichards86@gmail.com>
2016-06-08 23:35:57 +0000

    Adds menu icon

--- app/components/social.jsx
+++ app/components/social.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 
-import Menu from './menu';
-import MenuItem from './menuitem';
-
 export default class Social extends React.Component {
+    constructor() {
+        this.state = {toggled: false};
+    }
+    
+    handleClick() {
+        if (this.state.toggled) {
+            this.refs.social.style.display = "none";
+        }
+        else {
+            this.refs.social.style.display = "block";
+        }
+        
+        this.setState({toggled: !this.state.toggled});
+    }
+    
     render() {
         return (
             <aside>
-                <a href="https://github.com/dkrichards86/" target="_blank"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
-                <a href="http://codepen.io/dkrichards86/" target="_blank"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
-                <a href="www.linkedin.com/in/dkrichards" target="_blank"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
+                <i className="menu-icon" onClick={this.handleClick.bind(this)} />
+                <div className="soclial-links" rel="social">
+                    <a href="https://github.com/dkrichards86/" target="_blank"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
+                    <a href="http://codepen.io/dkrichards86/" target="_blank"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
+                    <a href="www.linkedin.com/in/dkrichards" target="_blank"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
+                </div>
             </aside>
         );
     }
