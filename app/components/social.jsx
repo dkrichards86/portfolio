585b7be048dbabb385d2491c0a7f571c75e12b4e
Keith Richards <dkrichards86@gmail.com>
2016-06-08 23:27:12 +0000

    Adds no follow to social links

--- app/components/social.jsx
+++ app/components/social.jsx
@@ -8,9 +8,9 @@ export default class Social extends React.Component {
     render() {
         return (
             <aside>
-                <a href="https://github.com/dkrichards86/"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
-                <a href="http://codepen.io/dkrichards86/"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
-                <a href="www.linkedin.com/in/dkrichards"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
+                <a href="https://github.com/dkrichards86/" target="_blank"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
+                <a href="http://codepen.io/dkrichards86/" target="_blank"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
+                <a href="www.linkedin.com/in/dkrichards" target="_blank"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
             </aside>
         );
     }
