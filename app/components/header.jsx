4350128b7e74e5584a53b152548846f5cbdd21b9
Keith Richards <dkrichards86@gmail.com>
2016-06-12 20:24:04 +0000

    Sticky header update

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -8,16 +8,18 @@ import Social from './social';
 export default class Header extends React.Component {
     render() {
         return (
-            <header>
-                <IndexLink to="/"><span className="headline">Keith Richards</span></IndexLink>
-                <Social />
-                <Menu>
-                    <MenuItem label="Life" route="/personal" icon="code" />
-                    <MenuItem label="Work" route="/professional" icon="person" />
-                    <MenuItem label="Resume" route="/resume" icon="work" />
-                    <MenuItem label="Projects" route="/projects" icon="work" />
-                </Menu>
-            </header>
+            <div className="sticky">
+                <header>
+                    <IndexLink to="/"><span className="headline">Keith Richards</span></IndexLink>
+                    <Social />
+                    <Menu>
+                        <MenuItem label="Life" route="/personal" icon="code" />
+                        <MenuItem label="Work" route="/professional" icon="person" />
+                        <MenuItem label="Resume" route="/resume" icon="work" />
+                        <MenuItem label="Projects" route="/projects" icon="work" />
+                    </Menu>
+                </header>
+            </div>
         );
     }
 }
