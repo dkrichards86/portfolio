1c66b96ddc6c511b150d4c368e4324b7964c25eb
Keith Richards <dkrichards86@gmail.com>
2016-07-10 22:09:20 +0000

    Fixes relative pathing issues.

--- components/social.jsx
+++ components/social.jsx
@@ -23,9 +23,9 @@ export default class Social extends React.Component {
             <aside>
                 <i className="menu-icon" onClick={this.handleClick.bind(this)} />
                 <div className="social-links" ref="social">
-                    <a href="https://github.com/dkrichards86/" target="_blank" title="View code samples on GitHub"><img src="./images/github.png" alt="Browse my work on GitHub" /></a>
-                    <a href="http://codepen.io/dkrichards86/" target="_blank" title="View code samples on CodePen"><img src="./images/codepen.png" alt="See some work on CodePen" /></a>
-                    <a href="http://www.linkedin.com/in/dkrichards" target="_blank" title="View my profile on LinkedIn"><img src="./images/linkedin.png" alt="View my profile on LinkedIn" /></a>
+                    <a href="https://github.com/dkrichards86/" target="_blank" title="View code samples on GitHub"><img src="/images/github.png" alt="Browse my work on GitHub" /></a>
+                    <a href="http://codepen.io/dkrichards86/" target="_blank" title="View code samples on CodePen"><img src="/images/codepen.png" alt="See some work on CodePen" /></a>
+                    <a href="http://www.linkedin.com/in/dkrichards" target="_blank" title="View my profile on LinkedIn"><img src="/images/linkedin.png" alt="View my profile on LinkedIn" /></a>
                 </div>
             </aside>
         );
