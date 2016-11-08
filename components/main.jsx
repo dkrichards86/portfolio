c588edaa5d968f430cd219f67ef0fccbeee0ae2c
Keith Richards <dkrichards86@gmail.com>
2016-11-08 22:30:36 +0000

    Loads CSS link after page render

--- components/main.jsx
+++ components/main.jsx
@@ -3,10 +3,19 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Header from './header';
 import Menu from './menu';
 
+import MetaManager from 'react-metamanager';
+
+const METATAGS = {
+    link: [
+        { href: "/dist/styles.css", rel: "stylesheet", type: "text/css" }
+    ]
+};
+
 export default class Main extends React.Component {
     render() {
         return (
             <div>
+                <MetaManager tags={METATAGS} />
                 <Header />
                 <div className="main">
                     <Menu />
